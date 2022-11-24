cb2c8bbdaeeefffa8a0d8249523f32159c9795da
Edinson <riveraedinson043@gmail.com>
2022-11-23 23:47:51 -0500

    Prueva tecnica itGlobers

--- src/components/Articulos1/Articulos1.js
+++ src/components/Articulos1/Articulos1.js
@@ -6,15 +6,6 @@ import styles from './Articulos1.module.css'
 const Articules1 = () => {
     return (
         <div>
-        <div>
-        <h2>Pufi RAIN</h2>
-        </div> 
-        <div>
-        <p>Descripcion del producto. Este es un texto simulado</p>
-        </div>
-        <div>
-        <h4>VER MAS</h4>
-        </div>   
         <div className={styles.articulos1}>
              {images.articules1.map(e => <img src={e.name} alt="imagen" />)}
         </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,17 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
+        
         <div>
-        <div className={styles.pufi}>
             <div className={styles.redesparr}>
             <h5>SIGUENOS EN</h5>
             </div>
             <div className={styles.redes}>
             {images.footer.map(e => <img src={e.name} alt="imagen" />)}
             </div>
-         <h1>Pufi</h1>
+            <div className={styles.pufi}>
+          <h1>Pufi</h1>
+         </div>
          <div className={styles.h6}>
          <h6>
             <span>PUFI RAIN</span>
@@ -29,15 +31,15 @@ const Footer = () => {
            <span>TERMINOS & CONDICIONES</span>
          </h6>
          </div>
-            <div className={styles.qr}>
-            <div className={styles.seguro}>   
+            {/* <div className={styles.seguro}>   
             <h6>COMPRA 100% SEGURA</h6>
-            </div>
+            </div> */}
+            <div className={styles.qr}>
             {images.qr.map(e => <img src={e.name} alt="imagen" />)}
             </div>
         
         </div>
-        </div>
+    
     );
 };
 
